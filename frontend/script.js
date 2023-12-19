334ac2623ca3a2fe27272a4d88b661e09adc2498
Gabor Balint <trondheim.bg@gmail.com>
2023-12-19 16:39:03 +0100

    fetch today's data

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,8 @@
 const loadEvent = async function() {
-
+    // Fetch the today's data
+    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+    const responseJson = await response.json();
+    console.log(responseJson)
 }
 
 window.addEventListener("load", loadEvent);
